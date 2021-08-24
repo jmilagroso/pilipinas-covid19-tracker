9f8202074f8ea8e4e52821a0987b49d8685d6d80
jmilagroso <j.milagroso@gmail.com>
2021-08-24 16:56:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ n_days_ago = today - timedelta(days=14)
 df = df.loc[df['location'] == 'Philippines']
 df = df.loc[df['date'] >= str(n_days_ago.date())]
 
-st.write("Philippines Covid-19 Cases (Past 2 Weeks)")
+st.write("Philippines Covid-19 Statistics")
 
 st.write("Source: https://covid.ourworldindata.org")
 
