f51f2497759ca09826f7a91472f4ebf369284e2a
jmilagroso <j.milagroso@gmail.com>
2022-01-04 10:18:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,20 +22,18 @@ def load_data():
 
 df = load_data()
 
-today = datetime.now() + timedelta(hours=8)
-n_days_ago_past = today - timedelta(days=15)
-n_days_ago_past60 = today - timedelta(days=60)
+today = datetime.now()
+n_days_ago_past = today - timedelta(days=60)
 
 df = df.loc[df['location'] == 'Philippines']
 df = df.loc[df['date'] >= str(n_days_ago.date())]
-df1 = df.loc[df['date'] >= str(n_days_ago_past60.date())]
 
 st.markdown("<h1 style='text-align: center;'>PH Covid-19 Tracker</h1>", unsafe_allow_html=True)
 
 st.write("Source: https://covid.ourworldindata.org")
 
 fig1 = px.bar(
-    df1, 
+    df, 
     x='date', 
     y='new_cases',
     color="new_cases",
@@ -45,7 +43,7 @@ fig1.update_layout(width=1240)
 st.plotly_chart(fig1)
 
 fig2 = px.bar(
-    df1, 
+    df, 
     x='date', 
     y='new_deaths',
     color="new_deaths",
@@ -54,6 +52,11 @@ fig2 = px.bar(
 )
 fig2.update_layout(width=1240)
 st.plotly_chart(fig2)
+
+n_days_ago_past = today - timedelta(days=15)
+df = load_data()
+df = df.loc[df['location'] == 'Philippines']
+df = df.loc[df['date'] >= str(n_days_ago.date())]
 
 base1 = alt.Chart(df).mark_bar().encode(
     x='monthdate(date):O',
@@ -72,7 +75,7 @@ text1 = base1.mark_text(
     text='total_tests:Q'
 )
 
-base2 = alt.Chart(df).mark_bar().encode(
+base2 = alt.Chart(df2).mark_bar().encode(
     x='monthdate(date):O',
     tooltip=['people_fully_vaccinated']
 ).properties(
@@ -92,8 +95,6 @@ text2 = base1.mark_text(
 with st.empty():
     (base1.encode(y='total_tests', color=alt.value("#ffb347")) + text1).properties(title=f'Total Number of Tests for the past 14 days') | (base2.encode(y='people_fully_vaccinated', color=alt.value("#228B22")) + text2).properties(title=f'Total Number of Fully Vaccinated for the past 14 days')
 
-
-today = datetime.now() + timedelta(hours=0)
 n_days_ago = today - timedelta(days=90)
 
 df = load_data()
